6b938dc134be6140f34fdcb59b0464ddcad7c8c8
BhinTatta <67475518+BhinTatta@users.noreply.github.com>
2022-07-16 20:15:31 +0530

    Time: 736 ms (8.67%), Space: 345.8 MB (5.05%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -24,8 +24,8 @@ public:
         bool ans;
         
         if(dp[i][sum]!=-1){
-            dp[i][sum]==1 ? ans = true : ans = false;
-            return ans;
+            //dp[i][sum]==1 ? ans = true : ans = false;
+            return dp[i][sum];
         }
         
         if(nums[i] > sum){
