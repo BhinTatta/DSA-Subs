be3c7b39c14c887db994c89d337f8a2c319e04bd
BhinTatta <67475518+BhinTatta@users.noreply.github.com>
2022-07-22 15:01:59 +0530

    Time: 31 ms (74.56%), Space: 13.5 MB (83.52%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -4,30 +4,27 @@ public:
         int n = graph.size();
         
         vector<int> color( n , 0);
-        queue<int> q;
         
         for(int i=0 ; i<n ; i++){
-            if(color[i]!=0) continue;
-            color[i] = 1;
-            q.push(i);
+            if(color[i] != 0) continue;
             
-            while(!q.empty()){
-                int node = q.front();
-                q.pop();
-                
-                for(auto adj : graph[node]){
-                    if( color[adj] == 0 ){
-                        color[adj] = -1 * color[node];
-                        q.push(adj);
-                    }
-                    else if( color[adj] == color[node] ){
-                        return false;
-                    }
-                }
-            }
-            
+            if(dfs( i , color , graph , 1 ) ) return false;
         }
         
         return true;
+    }
+    
+    bool dfs( int i , vector<int> &color , vector<vector<int>>& graph , int col ){
+        if( color[i] == col*-1 ) return true;
+        if(color[i]!=0) return false;
+        
+        color[i] = col;
+        
+        for(auto adj : graph[i]){
+            if(dfs( adj , color , graph , -1*col )) return true;
+        }
+        
+        return false;
+        
     }
 };
