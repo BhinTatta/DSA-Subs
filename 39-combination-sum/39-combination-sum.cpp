558afd2b095f93673e097257341a9518fda6cb42
BhinTatta <67475518+BhinTatta@users.noreply.github.com>
2022-07-23 15:33:04 +0530

    Time: 171 ms (17.87%), Space: 86.4 MB (8.84%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int n ;
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        n = nums.size();
+        int k = target;
+        
+        vector<vector<int>> ans;
+        vector<int>temp;
+        helper(nums , ans , temp , k , 0);
+        return ans;
+    }
+    
+    void helper(vector<int> &nums , vector<vector<int>> &ans, vector<int>temp , int sum , int i){
+        if( i >= n){
+            if(sum==0) ans.push_back(temp);
+            return;
+        }
+        
+        //take
+        if(nums[i] > sum) helper(nums , ans , temp , sum , i+1);
+        else{
+            helper(nums , ans , temp , sum , i+1);
+            temp.push_back(nums[i]);
+            helper(nums , ans , temp , sum - nums[i] , i);
+            temp.pop_back();
+        }
+        return;
+    }
+};
