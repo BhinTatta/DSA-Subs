0941223a27a7a92cfcb537aab10a010669499991
BhinTatta <67475518+BhinTatta@users.noreply.github.com>
2022-07-24 20:02:51 +0530

    Time: 95 ms (95.12%), Space: 15 MB (21.86%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -6,16 +6,11 @@ public:
         n = mat[0].size()-1;        
         int r = 0 , c = n;        
         if(k < mat[0][0] || k > mat[m][n]) return false;        
-        while( isbound(r,c) ){
+        while( r <= m && c >= 0 ){
             int curr = mat[r][c];
             if( k == curr ) return true;
             k > curr ? r++ : c-- ;
         }        
         return false;
-    }
-    
-    bool isbound(int a , int b){
-        if(a > m || a < 0 || b > n || b < 0) return false;
-        return true;
     }
 };
