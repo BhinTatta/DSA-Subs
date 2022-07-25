ebb5a9064d834c71f5cbf4ce0dbaf4eca3c6445c
BhinTatta <67475518+BhinTatta@users.noreply.github.com>
2022-07-25 17:03:25 +0530

    Time: 144 ms (45.78%), Space: 9.9 MB (97.68%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -3,25 +3,25 @@ public:
     int n , k;
     int coinChange(vector<int>& coins, int amount) {
         n = coins.size();
-        vector<vector<int>> dp( n+1 , vector<int>(amount+1 , -1));
-        for(int i = 0 ; i < amount+1 ; i++) dp[0][i] = INT_MAX-1;
-        dp[0][0]=0;
+        int dp[amount+1];
+        for(int i = 0 ; i < amount+1 ; i++) dp[i] = INT_MAX-1;
+        dp[0]=0;
         
         for(int i = 1 ; i < n+1 ; i++){
             for(int j = 0 ; j < amount+1 ; j++){
                 int take = INT_MAX, dont = INT_MAX;
                 if(coins[i-1]<=j){
-                    take = 1 + dp[i][j-coins[i-1]];
-                    dont = dp[i-1][j];
+                    take = 1 + dp[j-coins[i-1]];
+                    dont = dp[j];
                 }
                 else{
-                    dont = dp[i-1][j];
+                    dont = dp[j];
                 }             
-                dp[i][j] = min( take , dont);
+                dp[j] = min( take , dont);
             }
         }
         
-        return dp[n][amount]==INT_MAX-1 ? -1 : dp[n][amount];
+        return dp[amount]==INT_MAX-1 ? -1 : dp[amount];
     }
 
 };
