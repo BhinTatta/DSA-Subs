402f9ab03c8731ebc9fb6a1d63a2d0b5a56038f0
BhinTatta <67475518+BhinTatta@users.noreply.github.com>
2022-07-27 17:26:21 +0530

    Time: 26 ms (78.20%), Space: 14.3 MB (40.32%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
+    vector<int>ans;
+
     vector<int> findOrder(int n, vector<vector<int>>& prereq) {
         vector<vector<int>> adj(n);
-        vector<int> indg(n);
         for(int i = 0 ; i < prereq.size() ; i ++){
             adj[prereq[i][0]].push_back(prereq[i][1]);
-            indg[prereq[i][1]]++;
         }
-        queue<int> q;
+        vector<int> vis(n,0);
         for(int i = 0 ; i < n ; i++ ){
-            if(indg[i]==0) q.push(i);
+            if(vis[i]==0) dfs(adj , vis , i);
         }
-        
-        vector<int>ans;
-        while(!q.empty()){
-            int curr = q.front();
-            ans.push_back(curr);
-            q.pop();
-            
-            for(auto it : adj[curr]){
-                indg[it]--;
-                if(indg[it]==0) q.push(it);
-            }
-        }
-        reverse(ans.begin() , ans.end());
+
         vector<int> temp;
         return ans.size()==n ? ans : temp;
+    }
+    
+    void dfs(vector<vector<int>> &adj , vector<int> &vis , int i){
+        if(vis[i]==2) return;
+        vis[i] = 1;   
+        for(auto it : adj[i]){
+            if(vis[it]==1) return;
+            if(vis[it]==0) dfs(adj , vis , it);
+        }  
+        vis[i]=2;
+        ans.push_back(i);
     }
 };
