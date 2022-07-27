0404f9987c9b26ac9e7f52fe534ba7f31320be40
BhinTatta <67475518+BhinTatta@users.noreply.github.com>
2022-07-27 19:27:29 +0530

    Time: 1209 ms (8.57%), Space: 48.8 MB (16.59%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -3,24 +3,21 @@ public:
     int n;
     bool canJump(vector<int>& nums) {
         n = nums.size();
-        vector<int> dp(n,-1);
-        return dfs(nums , 0 , dp);
-    }
-    
-    bool dfs(vector<int> &nums , int i , vector<int>&dp){
-        if(i >= n) return false;
-        if(i == n-1 ) return true;
-        if(nums[i]==0) return false;
+        reverse(nums.begin() , nums.end());
+        vector<bool> dp(n,false);
+        dp[0] = true;
         
-        if(dp[i]!=-1) return dp[i];
-        bool ans = false;
-        for(int j = 1 ; j <= nums[i] ; j++){
-            bool curr = dfs(nums , i+j , dp);
-            if(curr){
-                return dp[i] = true;
+        for(int i = 1 ; i < n ; i++){
+            int x = 1;
+            while(i-x>=0 && x <= nums[i]){
+                if(dp[i-x]){
+                    dp[i]= true;
+                    break;
+                }
+                x++;
             }
         }
-        
-        return dp[i] = false;
+        return dp[n-1];
     }
+
 };
