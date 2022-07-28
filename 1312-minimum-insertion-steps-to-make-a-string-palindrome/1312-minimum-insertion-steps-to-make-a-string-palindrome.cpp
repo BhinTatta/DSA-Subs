60d179f2602b2035f7b128f093be02f8626d6e2c
BhinTatta <67475518+BhinTatta@users.noreply.github.com>
2022-07-28 17:07:20 +0530

    Time: 52 ms (70.69%), Space: 7.5 MB (83.35%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -6,7 +6,8 @@ public:
         string s2 = s;
         int n = s1.length();
         int m = n;
-        vector<vector<int>> dp(n+1 , vector<int>(m+1,0));
+        int dp[n+1][m+1];
+        memset(dp , 0, sizeof(dp));
         
         for(int i = 1 ; i < n+1 ; i++){
             for(int j = 1 ; j < m+1 ; j++){
