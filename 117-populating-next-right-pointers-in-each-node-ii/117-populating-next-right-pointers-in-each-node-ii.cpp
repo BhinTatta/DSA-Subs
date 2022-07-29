28b93c0fec612c5e03084583198233d1b2a9e3ac
BhinTatta <67475518+BhinTatta@users.noreply.github.com>
2022-07-29 18:17:26 +0530

    Time: 34 ms (6.48%), Space: 18.1 MB (6.06%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.cpp
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.cpp
@@ -28,16 +28,18 @@ public:
     
     void bfs(queue<Node*>q){
         if(q.empty()) return;
-        queue<Node*> newq;
         Node* to_point = NULL;
-        while(!q.empty()){
+        int i = 0;
+        int n = q.size();
+        while(i<n){
             Node* curr = q.front();
             curr->next = to_point;
             to_point = curr;
             q.pop();
-            if(curr->right) newq.push(curr->right);
-            if(curr->left) newq.push(curr->left);
+            if(curr->right) q.push(curr->right);
+            if(curr->left) q.push(curr->left);
+            i++;
         }
-        bfs(newq);
+        bfs(q);
     }
 };
