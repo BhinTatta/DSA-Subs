3f385342b406e4d546b6483063bdb43d4f47abf7
BhinTatta <67475518+BhinTatta@users.noreply.github.com>
2022-08-03 17:54:01 +0530

    Time: 80 ms (32.60%), Space: 19 MB (8.94%) - LeetHub

--- 143-reorder-list/143-reorder-list.cpp
+++ 143-reorder-list/143-reorder-list.cpp
@@ -14,27 +14,23 @@ public:
         if(!head->next || !head->next->next) return;
         vector<ListNode*> arr;
         ListNode* curr = head;
-        
+        int size = 0;
         while(curr!=nullptr){
             arr.push_back(curr);
             curr = curr->next;
+            size++;
         }
         
-        int i = 0 , j = arr.size()-1;
-        int last;
+        int i = 0 , j = size-1;
+        int last;  //keeps track of last node, as it has to be pointed to null;
         while(i<j){
             arr[i]->next = arr[j];
-            // cout<<arr[i]->val <<"  -> "<<arr[j]->val<<endl;
-            // cout<<"i & j = "<<i<<" "<<j<<endl;
             i++;
             last = j;
             if(j-i>0){
                 arr[j]->next = arr[i];
                 last = i;
-                // cout<<arr[j]->val <<"  -> "<<arr[i]->val<<endl;
-                // cout<<"i & j = "<<i<<" "<<j<<endl;
                 j--;
-    
             }
         }
         arr[last]->next = nullptr;
