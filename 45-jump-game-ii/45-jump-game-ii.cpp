6f1a5a3894e6ddbb739db57619e8669f6ccc795b
BhinTatta <67475518+BhinTatta@users.noreply.github.com>
2022-08-04 13:04:25 +0530

    Time: 269 ms (42.42%), Space: 17.3 MB (28.09%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n+1 , -1);
-        return jumper(nums , n , 0 , dp) - 1;
-    }
-    
-    int jumper(vector<int>&nums , int n , int i , vector<int> &dp){
-        if(i>=n-1) return 1;
-        if(dp[i]!=-1) return dp[i];
-        int mini = INT_MAX-1;
-        for(int j = 1 ; j <= nums[i] ; j++){
-            int curr = 1 + jumper(nums , n , i+j , dp);
-            mini = min(mini , curr);
+        vector<int> dp(n+1 , 0);
+        for(int i = n-2 ; i >= 0 ; i--){
+            int mini = INT_MAX-1;
+            for(int j = 1 ; j <= nums[i] && i+j < n  ; j++){
+                int curr = 1 + dp[i+j];
+                mini = min(mini , curr);
+            }
+            dp[i]=mini;
         }
-        return dp[i] = mini;
+        return dp[0];
     }
+
 };
