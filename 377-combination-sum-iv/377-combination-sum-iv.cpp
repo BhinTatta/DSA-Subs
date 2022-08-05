0d11ff57fc20e995c70c146b9284bf9c2e5cbc1b
BhinTatta <67475518+BhinTatta@users.noreply.github.com>
2022-08-05 12:47:50 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (30.10%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -3,22 +3,22 @@ public:
     int combinationSum4(vector<int>& nums, int target) {
         int n = nums.size();
         int ans = 0;
-        vector<vector<int>> dp(n+1 , vector<int>(target+1,-1));
+        vector<int> dp(target+1,-1);
         ans += solver(nums,target,n,0,dp);
 
         return ans;
     }
     
-    int solver(vector<int>& nums, int target, int n, int i, vector<vector<int>> &dp){
+    int solver(vector<int>& nums, int target, int n, int i, vector<int> &dp){
         if(target==0) return 1;
         if(i >= n) return 0;
-        if(dp[i][target] != -1) return dp[i][target];
+        if(dp[target] != -1) return dp[target];
         int ans = 0;
         for(int k = 0 ; k < n ; k++){
             if(nums[k]<=target){
                 ans += solver(nums, target-nums[k] , n, k , dp);
             }
         }
-        return dp[i][target] = ans;
+        return dp[target] = ans;
     }
 };
