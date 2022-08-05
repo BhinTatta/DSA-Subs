29911547e748d44d13884034cc5b16acac109b21
BhinTatta <67475518+BhinTatta@users.noreply.github.com>
2022-08-05 13:35:59 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (19.55%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -4,14 +4,13 @@ public:
     vector<vector<int>> combinationSum3(int dig, int req) {
         maxlen = dig;
         req_sum = req;
-        
         vector<vector<int>> ans;
         vector<int>temp;
         solver(ans,temp , req_sum , 0 , 1 );
         return ans;
     }
     
-    void solver(vector<vector<int>> &ans ,vector<int>temp, int sum , int curr_len, int i){
+    void solver(vector<vector<int>> &ans ,vector<int>&temp, int sum , int curr_len, int i){
         if(curr_len == maxlen && sum==0){
             ans.push_back(temp);
             return;
@@ -22,6 +21,7 @@ public:
             solver(ans,temp,sum,curr_len,i+1);
             temp.push_back(i);
             solver(ans,temp,sum-i,curr_len+1,i+1);
+            temp.pop_back();
         }
         else{
             solver(ans,temp,sum,curr_len,i+1);
