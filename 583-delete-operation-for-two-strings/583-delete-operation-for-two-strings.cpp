ffa18e0f3dd09a60903b660e52c7427e2375f06d
Kartik <67475518+BhinTatta@users.noreply.github.com>
2022-08-08 17:54:31 +0530

    Time: 19 ms (77.79%), Space: 7.5 MB (85.58%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -3,8 +3,9 @@ public:
     int minDistance(string s, string t) {
         int n = s.length();
         int m = t.length();
-        
-        vector<vector<int>> dp(n+1 , vector<int>(m+1 , 0));
+        int dp[n+1][m+1];
+        memset(dp,0,sizeof(dp));
+
         for(int i = 1 ; i < n+1 ; i++){
             for(int j = 1 ; j < m+1 ; j++){
                 if(s[i-1]==t[j-1]){
