ec1d558a105e38fb7475f64be6e56699255348df
Kartik <67475518+BhinTatta@users.noreply.github.com>
2022-08-08 17:17:10 +0530

    Time: 17 ms (29.23%), Space: 13.7 MB (63.47%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,46 +1,9 @@
 class Solution {
 public:
-    int n;
     vector<int> searchRange(vector<int>& nums, int target) {
-        n = nums.size();
-        if(n==0) return vector<int>{-1,-1};
-        int left = findmin(nums,target);
-        int right = findmax(nums,target);
-        vector<int> ans = {left,right};
-        return ans;
-    }
-    
-private:
-    int findmin(vector<int>& nums, int target){
-        int minpos = n;
-        
-        int l = 0 , r = n-1;
-        while(l<=r){
-            int mid = (l+r)/2;
-            int curr = nums[mid];
-            if(curr<target) l = mid+1;
-            else if(curr>target) r = mid-1;
-            else if(curr==target){
-                minpos = min(mid , minpos);
-                r = mid-1;
-            }
-        }
-        return minpos == n ? -1 : minpos;
-    }
-    int findmax(vector<int>& nums, int target){
-        int maxpos = -1;
-        
-        int l = 0 , r = n-1;
-        while(l<=r){
-            int mid = (l+r)/2;
-            int curr = nums[mid];
-            if(curr<target) l = mid+1;
-            else if(curr>target) r = mid-1;
-            else if(curr==target){
-                maxpos = max(mid , maxpos);
-                l = mid+1;
-            }
-        }
-        return maxpos == -1 ? -1 : maxpos;
+        vector<int>ans;
+        int lower = lower_bound(nums.begin() , nums.end() , target) - nums.begin();
+        int upper = upper_bound(nums.begin() , nums.end() , target) - nums.begin();
+        return (lower<nums.size() && nums[lower]==target) ?ans = {lower,upper-1} : ans = {-1,-1};
     }
 };
