ee5c8e336d1c5e3d623c40cdb87d2a71571ce857
Kartik <67475518+BhinTatta@users.noreply.github.com>
2022-08-17 13:13:27 +0530

    Time: 159 ms (61.96%), Space: 75.8 MB (5.95%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -4,21 +4,15 @@ public:
     int maxSubArray(vector<int>& nums) {
         n = nums.size();
         vector<vector<int>> dp(2 , vector<int>(n+1,-1));
-        return helper(nums, 0 , false , dp);
-    }
-    
-    int helper(vector<int>&nums, int i , bool state , vector<vector<int>> &dp){
-        if(i==n){
-            return state==true ? 0 : -1e5;
+        dp[0][0] = nums[0];
+        dp[1][0] = nums[0];
+        
+        for(int i = 1 ; i < n ; i++){
+            dp[1][i] = max(nums[i] , nums[i]+dp[1][i-1]);
+            
+            dp[0][i] = max(dp[0][i-1] , dp[1][i]  );
         }
-        if(dp[state][i]!=-1) return dp[state][i];
-        int take , dont;
-        
-        if(state){
-            return dp[state][i] = max(0 , nums[i] + helper(nums,i+1,true,dp));
-        }
-        
-        
-        return dp[state][i] = max( helper(nums,i+1,false,dp) , nums[i]+helper(nums,i+1,true,dp));
+        return dp[0][n-1];
     }
+
 };
