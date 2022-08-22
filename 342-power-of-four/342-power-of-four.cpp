4355f79407a5317cfb3d826730d6b76625ef021c
Kartik <67475518+BhinTatta@users.noreply.github.com>
2022-08-22 11:31:39 +0530

    Time: 6 ms (19.21%), Space: 6 MB (24.97%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
-    bool isPowerOfFour(int n) {
-        if (n<=0) return 0;
-        long long lim = INT_MAX;
-        long long curr = 1;
-        while(curr <= n || curr < lim){
-            if(curr == n ) return true;
-            curr = 4*curr;
-        }
-        return false;
+    bool isPowerOfFour(int num) {
+        return num > 0 && (num&(num-1)) == 0 && (num & 0x55555555) != 0;
     }
 };
