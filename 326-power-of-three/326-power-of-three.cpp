61d48b30c03955a863eaf9128e207f613297a9b3
Kartik <67475518+BhinTatta@users.noreply.github.com>
2022-08-24 12:47:06 +0530

    Time: 63 ms (5.42%), Space: 6.5 MB (7.72%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        int maxi = INT_MAX;
-        long long curr = 1;
-        while(curr <= n && curr<maxi){
-            if(curr == n) return true;
-            curr *= 3;
+        if(n<1) return 0;
+        string base = "";
+        while(n){
+            string dig = to_string(n%3);
+            base+=dig;
+            n/=3;
+        }      
+        for(int i = 0 ; i < base.length()-1 ;i++){
+            if(base[i]!='0') return 0;
         }
-        return 0;
+        
+        return base[base.length()-1]=='1';
     }
 };
