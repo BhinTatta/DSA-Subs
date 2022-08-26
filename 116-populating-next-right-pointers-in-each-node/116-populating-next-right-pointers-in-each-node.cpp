6b34adcc725556c9185e4dd8ee5866ace646a052
Kartik <67475518+BhinTatta@users.noreply.github.com>
2022-08-26 17:07:52 +0530

    Time: 30 ms (56.22%), Space: 16.6 MB (99.60%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
@@ -20,13 +20,17 @@ class Solution {
 public:
     Node* connect(Node* root) {
         if(!root) return root;
-        
-        if(root->left) root->left->next = root->right;
-        if(root->right && root->next) root->right->next = root->next->left;
-        connect(root->left);
-        connect(root->right);
-        
-        return root;
+        Node* ans = root;
+        while(root->left){
+            Node* curr = root;
+            while(curr){
+                curr->left->next = curr->right;
+                if(curr->next) curr->right->next = curr->next->left;
+                curr = curr->next;
+            }
+            root = root->left;
+        }
+        return ans;
     }
     
 };
