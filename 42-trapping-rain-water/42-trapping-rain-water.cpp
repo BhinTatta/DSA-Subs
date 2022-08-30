16a8db3ee7988beee54cf4b7c1aded091f8f2a0c
Kartik <67475518+BhinTatta@users.noreply.github.com>
2022-08-30 12:39:17 +0530

    Time: 14 ms (93.77%), Space: 19.6 MB (99.94%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -2,23 +2,35 @@ class Solution {
 public:
     int trap(vector<int>& nums) {
         int n = nums.size();
-        int maxi = nums[0];
-        vector<int> left(n);
-        for(int i = 0 ; i < n ; i ++){
-            maxi = max(maxi , nums[i]);
-            left[i] = maxi;
-        }
-        maxi = nums[n-1];
-        vector<int> right(n);
-        for(int i = n-1 ; i >= 0 ; i--){
-            maxi = max(maxi , nums[i]);
-            right[i] = maxi;
-        }
-        
+        if(n<=2) return 0;
         int ans = 0;
-        for(int i = 0 ; i < n ; i++){
-            int water =  min(left[i] , right[i]) - nums[i];
-            if(water>0) ans += water;
+        int l = 1 , r = n-2;
+        int lmax = nums[0] , rmax = nums[n-1];
+        
+        while(l<=r){
+            
+            int left = nums[l], right = nums[r];
+            
+            if(lmax <= rmax){
+                if(left>lmax){
+                    lmax = left;
+                }
+                else{
+                    ans += lmax-left;
+                }
+                l++;
+            }
+            
+            else if(lmax > rmax){
+                if(right > rmax){
+                    rmax = right;
+                }
+                else{
+                    ans += rmax-right;
+                }
+                r--;
+            }
+            //cout<<ans<<endl;
         }
         
         return ans;
