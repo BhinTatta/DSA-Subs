4f5627f714c89b9ff9afb8635070a340322f60e5
Kartik <67475518+BhinTatta@users.noreply.github.com>
2022-09-01 18:42:52 +0530

    Time: 47 ms (53.83%), Space: 25.1 MB (57.12%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -3,15 +3,8 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         k = k%n;
-        vector<int> lund(n);
-        
-        for(int i = 0 ; i < k ; i++){
-            lund[i] = nums[n-k+i];
-            
-        }
-        for(int i = k ; i < n ; i++){
-            lund[i] = nums[i-k];
-        }
-        nums = lund;
+        reverse(nums.begin() , nums.end());
+        reverse(nums.begin() , nums.begin()+k);
+        reverse(nums.begin()+k , nums.end());
     }
 };
