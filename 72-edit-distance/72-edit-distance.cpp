46bad553bffdfad246d235503c432c19bb1e0edf
Kartik <67475518+BhinTatta@users.noreply.github.com>
2022-09-09 18:43:19 +0530

    Time: 37 ms (29.32%), Space: 9.1 MB (40.47%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -3,27 +3,23 @@ public:
     int minDistance(string s1, string s2) {
         int n = s1.length();
         int m = s2.length();
-        vector<vector<int>> dp(n+1 , vector<int>(m+1,-1));
-        return helper(s1,s2,n-1,m-1,dp);
+        if(n==0 || m==0) return max(n,m);
+        vector<vector<int>> dp(n+1 , vector<int>(m+1,0));
+        for(int i = 0 ; i <= n ; i ++) dp[i][0] = i;
+        for(int i = 0 ; i <= m ; i++) dp[0][i] = i;
+        
+        for(int i = 1 ; i < n+1 ; i++){
+            for(int j = 1 ; j < m+1 ; j++){
+                if(s1[i-1]==s2[j-1]) dp[i][j] = dp[i-1][j-1];
+                else{
+                    int del = 1 + dp[i-1][j];
+                    int ins = 1 + dp[i][j-1];
+                    int rep = 1 + dp[i-1][j-1];        
+                    dp[i][j] = min(del , min(ins , rep));
+                }
+            }
+        }
+        return dp[n][m];
     }
     
-    int helper(string &s1, string &s2 , int i , int j ,  vector<vector<int>> &dp){
-        if(i<0 || j<0){
-            if(i<0 && j<0) return 0;
-            else return max(i+1,j+1);
-        }
-        
-        if(dp[i][j]!=-1) return dp[i][j];
-        
-        int ans;
-        if(s1[i]==s2[j]) ans = helper(s1,s2,i-1,j-1,dp);
-        else{
-            int del = 1 + helper(s1,s2,i-1,j,dp);
-            int ins = 1 + helper(s1,s2,i,j-1,dp);
-            int rep = 1 + helper(s1,s2,i-1,j-1,dp);
-            
-            ans = min(del , min(ins , rep));
-        }
-        return dp[i][j] = ans;
-    }
 };
