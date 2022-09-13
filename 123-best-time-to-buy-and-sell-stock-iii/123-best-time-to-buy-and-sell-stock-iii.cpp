93561120c183667f75e91dae2a719162d86faed8
Kartik <67475518+BhinTatta@users.noreply.github.com>
2022-09-13 12:58:50 +0530

    Time: 147 ms (98.05%), Space: 75.3 MB (78.82%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -4,26 +4,16 @@ public:
         int n = nums.size();
         if(n==1) return 0;
         
-        vector<int> left(n) , right(n);
-        left[0] = 0;
-        right[n-1] = 0;
+        int buy1 = INT_MAX , buy2 = buy1;
+        int profit1 = INT_MIN , profit2 = INT_MIN;
         
-        int minleft = nums[0];
-        for(int i = 1; i < n ; i++){
-            minleft = min(minleft , nums[i]);
-            left[i] = max(left[i-1]  , nums[i]-minleft);
+        for(int i = 0 ; i < n ; i++){
+            buy1 = min(buy1 , nums[i]);
+            profit1 = max(profit1 , nums[i]-buy1);
+            buy2 = min(buy2 , nums[i]-profit1);
+            profit2 = max(profit2 , nums[i]-buy2);
         }
         
-        int maxright = nums[n-1];
-        for(int i = n-2 ; i >= 0 ; i--){
-            maxright = max(maxright , nums[i]);
-            right[i] = max(right[i+1] , maxright - nums[i]);
-        }
-        
-        int ans = 0;
-        for(int i = 0 ; i < n ; i ++){            
-            ans = max(ans , right[i]+left[i]);
-        }
-        return ans;
+        return profit2;
     }
 };
