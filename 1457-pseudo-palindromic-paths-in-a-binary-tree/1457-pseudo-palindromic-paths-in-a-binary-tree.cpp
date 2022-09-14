17ebb2e2b005f153dac189e63390cc1d497ed194
Kartik <67475518+BhinTatta@users.noreply.github.com>
2022-09-14 19:18:02 +0530

    Time: 732 ms (29.47%), Space: 178.6 MB (97.90%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -12,35 +12,25 @@
 class Solution {
 
 public:
-    long long ans = 0;
+    int ans = 0;
     int pseudoPalindromicPaths (TreeNode* root) {
-        vector<int> mp(10,0);
-        helper(root , mp);
+        helper(root);
         return ans;
     }    
     
-    void helper(TreeNode* root , vector<int> mp){
+    void helper(TreeNode* root , int count = 0){
         if(!root){
             return;
         }
-        
-        int pos = root->val;
-        mp[pos]+=1;    
+        count = count^(1<<root->val); 
         
         if(!root->left && !root->right){
-            if(ispalind(mp)) ans++;
-            return;
-        }
+            if( (count&count-1) == 0) ans++;
+        }  
+        helper(root->left , count);
+        helper(root->right , count);  
         
-        helper(root->left , mp);
-        helper(root->right , mp);     
+        count=count^(1<<root->val);
     }
     
-    bool ispalind(vector<int>&mp){
-        int odd = 0;
-        for(int x : mp){
-            odd += x%2;
-        }
-        return odd<2;
-    }
 };
