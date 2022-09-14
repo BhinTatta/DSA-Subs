4a7fec8ed0c6e646580fee1c1886ae6d3c20e5fe
Kartik <67475518+BhinTatta@users.noreply.github.com>
2022-09-14 19:03:48 +0530

    Time: 1705 ms (7.69%), Space: 448.9 MB (9.79%) - LeetHub

---
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+
+public:
+    long long ans = 0;
+    int pseudoPalindromicPaths (TreeNode* root) {
+        vector<int> mp(10,0);
+        helper(root , mp);
+        return ans;
+    }    
+    
+    void helper(TreeNode* root , vector<int> mp){
+        if(!root){
+            return;
+        }
+        
+        int pos = root->val;
+        mp[pos]+=1;    
+        
+        if(!root->left && !root->right){
+            if(ispalind(mp)) ans++;
+        }
+        
+        helper(root->left , mp);
+        helper(root->right , mp);     
+    }
+    
+    bool ispalind(vector<int>&mp){
+        int odd = 0;
+        for(int x : mp){
+            odd += x%2;
+        }
+        return odd<2;
+    }
+};
