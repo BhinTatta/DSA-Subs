237027dcdb3226b0a5dfb8aae77a7b9d882a5147
Kartik <67475518+BhinTatta@users.noreply.github.com>
2022-09-20 00:39:21 +0530

    Time: 8 ms (37.84%), Space: 10.3 MB (22.54%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
@@ -4,25 +4,18 @@ public:
         int n = nums.size();
         int l = 0 , r = n-1;
         
-        while(r-l>1){
+        if(n==1 || nums[0]<=nums[n-1]) return nums[0];
+        
+        while(r-l > 1){
             //cout<<l<<" "<<r<<endl;
             int mid = (l+r)/2;
-            bool left = isokay(nums , l , mid);
-            bool right = isokay(nums , mid , r);
             
-            if(left && right) return nums[l];
+            if(nums[mid-1] > nums[mid] ) return nums[mid];
             
-            if(left) l = mid;
-            else if(right) r = mid;
+            if(nums[mid] > nums[r]) l = mid+1;
+            else r = mid-1;
         }
         
-        return min(nums[l],nums[r]);
-    }
-    
-    bool isokay(vector<int>& nums , int l , int r){
-        int mid = (l+r)/2;
-        
-        if( nums[l] <= nums[mid] && nums[mid]<= nums[r]) return true;
-        return false;
+        return min(nums[l] , nums[r]);
     }
 };
