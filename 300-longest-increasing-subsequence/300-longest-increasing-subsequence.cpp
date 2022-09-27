87694a311337c58fe0f46739a9defee7101d79e5
Kartik <67475518+BhinTatta@users.noreply.github.com>
2022-09-27 19:09:29 +0530

    Time: 2151 ms (6.11%), Space: 38.1 MB (15.44%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
+    int dp[2505][2505];
+    
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> ans;
-        int len = 0;
-        for(int i = 0 ; i < n ; i++){
-            int curr = nums[i];
-            int lower = lower_bound(ans.begin(),ans.end(),curr) - ans.begin();
-            if(lower==ans.size()){
-                ans.push_back(curr);
-            }
-            else ans[lower_bound(ans.begin(),ans.end(),ans[lower]) - ans.begin()]=curr;
+        memset(dp , -1, sizeof(dp));
+        int ans = 0,  n = nums.size();
+        for(int i = 0 ; i < n-1 ; i++){
+            ans = max( ans , helper(nums , i+1 , i) );
         }
-        return ans.size();
+        return 1 + ans;
+    }
+    
+    int helper(vector<int> &nums , int i , int prev){
+        if(i >= nums.size()) return 0;
+        
+        if(dp[i][prev]!=-1) return dp[i][prev];
+        
+        int a = helper(nums , i+1 , prev);
+        int b = 0;
+        if(nums[i]>nums[prev]){
+            b = 1+helper(nums,i+1,i);
+        }
+        return dp[i][prev] = max(a,b);
     }
 };
