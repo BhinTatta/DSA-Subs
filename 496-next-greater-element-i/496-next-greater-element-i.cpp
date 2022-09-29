2659b05a42fe73c2abcd8c92fde189a2b6314b76
Kartik <67475518+BhinTatta@users.noreply.github.com>
2022-09-29 19:19:05 +0530

    Time: 11 ms (56.47%), Space: 9 MB (32.03%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -2,34 +2,20 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int ,int> mp;
-        mp[nums[n-1]] = -1;
-        int i = n-2;
+        stack<int> st;
+        unordered_map<int,int> mp;
         
-        while(i>=0){
+        for(int i = n-1 ; i >= 0 ; i--){
             int curr = nums[i];
-            int j = i+1;
-            while(j<n){
-                if(nums[j]>curr){
-                    mp[curr]=nums[j];
-                    break;
-                }
-                else if(nums[j]<curr){
-                    if(mp[nums[j]]==-1){
-                        mp[curr] = -1;
-                        break;
-                    }
-                }
-                j++;
-            }
-            if(j==n) mp[curr]=-1;
-            //cout<<mp[curr]<<" ";
-            i--;
+            while(!st.empty() && st.top()<=curr) st.pop();
+            if(st.empty()) mp[curr] = -1;
+            else mp[curr] = st.top();
+            st.push(curr);
         }
         
         vector<int>ans;
-        for(int it : nums1){
-            ans.push_back( mp[it] );
+        for(auto it : nums1){
+            ans.push_back(mp[it]);
         }
         
         return ans;
