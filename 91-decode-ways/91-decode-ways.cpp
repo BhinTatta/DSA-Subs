d27d04a4453869a52a9d01d69511aba25323cb82
Kartik <67475518+BhinTatta@users.noreply.github.com>
2022-10-01 16:19:14 +0530

    Time: 5 ms (33.84%), Space: 6.1 MB (93.04%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
-    int numDecodings(const string& s) {
-        int n = s.size();
-        vector<int> dp(n+1, 0);
-        dp[n] = 1;
-        for (int i = n - 1; i >= 0; --i) {
-            if (s[i] != '0') 
-                dp[i] += dp[i+1];
-            if (i+1 < s.size() && (s[i] == '1' || s[i] == '2' && s[i+1] <= '6')) 
-                dp[i] += dp[i+2];
-        }
+    int n;
+    int dp[101];
+    int numDecodings(string s) {
+        n = s.length();
+        memset(dp,-1,sizeof(dp));
+        if(s[0]=='0') return 0;
+        solver(s , 0);
         return dp[0];
+    }
+    
+    int solver(string &s , int i){
+        if(i >= n) return 1;
+        if(s[i]=='0') return 0;
+        if(dp[i]!=-1) return dp[i];
+        int one = 0 , two = 0;
+        if(s[i]!='0') one = solver(s,i+1);
+        if(i+1<n && (s[i]=='1')){
+            two = solver(s,i+2);
+        }
+        if(i+1<n && (s[i]=='2' && (s[i+1]-'0' <= 6))){
+            two = solver(s,i+2);
+        }
+        return dp[i]=one+two;
     }
 };
