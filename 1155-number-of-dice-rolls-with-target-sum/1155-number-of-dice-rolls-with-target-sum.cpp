523d67186198e4da79d723625f58824e1033053a
Kartik <67475518+BhinTatta@users.noreply.github.com>
2022-10-02 23:00:15 +0530

    Time: 25 ms (82.85%), Space: 6.1 MB (87.41%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
-    //copying it, going to santosh dhaba, will do it tonight
-    typedef long long ll;
-    ll mod = 1e9 + 7;
-    ll dp[35][1005];
-
-    ll recurse(int turn, int target, int n, int k) {
-        if(target < 0) return 0;
-        if(turn == n) {
-            return (target == 0);
-        }
-        if(dp[turn][target] != -1) return dp[turn][target];
-        ll ways = 0;
-        for(int i = 1; i <= min(k, target); i++) {
-            ways = (ways + recurse(turn+1, target - i, n, k)) % mod;
-        } 
-        return dp[turn][target] = ways;
-    }
+    int mod = 1e9 + 7;
+    int dp[35][1005];
+    
     int numRollsToTarget(int n, int k, int target) {
-        memset(dp, -1, sizeof dp);
-        return recurse(0, target, n, k);
+        memset(dp , -1 ,sizeof(dp));
+        long long int ans = 0;
+        for(int i = 1 ; i <= k ; i++){
+            ans += helper(n-1 , target-i , k) % mod;
+        }
+        return ans%mod;
+    }
+    
+    int helper(int n , int target , int k ){
+        if(n == 0 || target<=0){
+            if(target==0 && n==0) return 1;
+            return 0;
+        }
+        if(dp[n][target]!=-1) return dp[n][target];
+        long long int ans = 0;
+        for(int i = 1 ; i <= k ; i++ ){
+            ans += helper(n-1 , target-i , k) % mod;
+        }
+        
+        return dp[n][target] = ans%mod;
     }
 };
