0fa2a08f0995e472fe05fd60a310db9787ffece0
Kartik <67475518+BhinTatta@users.noreply.github.com>
2022-10-04 14:19:47 +0530

    Time: 419 ms (12.81%), Space: 12.7 MB (98.81%) - LeetHub

--- 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.cpp
+++ 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.cpp
@@ -1,48 +1,32 @@
 class Solution {
 public:
     int findNumberOfLIS(vector<int>& nums) {
-        vector<pair<int,int>>dp; 
-        dp.push_back({1,1});     
-        for(int i=1;i<nums.size();i++)
-        {
-            int len=0;
-            int count=0;
-            for(int j=0;j<i;j++)
-            {
-                if(nums[j]<nums[i])  
-                {                                        
-                    if(len==dp[j].first)   //if max length we found till now is equal to current length
-                    {
-                        count+=dp[j].second;// we increment count( no. of combinations)
-                    }
-                    else if(dp[j].first>len) //if greater len is found
-                    {
-                        count=dp[j].second; 
-                        len=dp[j].first;         //then we set count to this seq count and len(maximum len) to its length 
-                    }
+        int n = nums.size();
+        int dp[n];
+        int cnt[n];
+        for(int i = 0 ; i < n ; i++){
+            dp[i]=1;
+            cnt[i]=1;
+        }
+        
+        int ans = 1;
+        for(int i = 1 ; i < n ; i++){
+            for(int j = 0 ; j < i ; j++){
+                if(nums[i]>nums[j] && dp[i]<dp[j]+1){
+                    dp[i] = dp[j]+1;
+                    cnt[i] = cnt[j];
+                }
+                else if(nums[i]>nums[j] && dp[i]==1+dp[j]){
+                    cnt[i] += cnt[j];
                 }
             }
-            if(count==0)
-            {
-                dp.push_back({len+1,1});
-            }
-            else 
-                dp.push_back({len+1,count});
+            ans = max(dp[i],ans);
         }
-        int len=0;
-        int count=0;
-        for(int i=0;i<dp.size();i++) 
-        {
-            if(len<dp[i].first)
-            {
-                len=dp[i].first;
-                count=dp[i].second;
-            }
-            else if(len==dp[i].first)
-            {
-                count+=dp[i].second;
-            }
+        
+        int num = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(dp[i]==ans) num += cnt[i];
         }
-        return count;
+        return num;
     }
 };
