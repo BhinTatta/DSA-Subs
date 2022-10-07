baca77cdd1379cdd6f938cca52879999964a62e0
Kartik <67475518+BhinTatta@users.noreply.github.com>
2022-10-07 15:37:45 +0530

    Time: 585 ms (68.95%), Space: 9 MB (82.36%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.cpp
+++ 312-burst-balloons/312-burst-balloons.cpp
@@ -5,25 +5,23 @@ public:
     int maxCoins(vector<int>& arr) {
         memset(dp,-1,sizeof(dp));
         n = arr.size();
-        vector<int> nums = {1};
-        for(int x : arr) nums.push_back(x);
-        nums.push_back(1);
-        return helper(nums,1,n);
+        arr.insert(arr.begin() , 1);
+        arr.push_back(1);
+        return helper(arr,1,n);
     }
     
     int helper(vector<int>&nums , int i , int j){
         if(i>j) return 0;
         if(i==j){
-            return nums[i]*nums[i-1]*nums[j+1];          
+            return nums[i]*nums[i-1]*nums[i+1];          
         };
         if(dp[i][j]!=-1) return dp[i][j];
-        int maxi = 0 , brek = 0;
+        int maxi = 0;
         for(int k = i ; k <= j ; k++){
             int curr = nums[k]*nums[i-1]*nums[j+1];
             int cost = helper(nums,i,k-1)+helper(nums,k+1,j)+curr;
             maxi = max(cost,maxi);           
         }
-        //cout<<nums[brek]<<endl;
         return dp[i][j] = maxi;
     }
 };
