00124761f8b06c192b174cf47a6d60046f08a068
Kartik <67475518+BhinTatta@users.noreply.github.com>
2022-10-13 17:15:19 +0530

    Time: 178 ms (31.74%), Space: 19.3 MB (73.01%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,33 +1,49 @@
 class Solution {
 public:
-int shortestPathBinaryMatrix(vector<vector<int>>& g, int steps = 0) {
-  queue<pair<int, int>> q;
-  q.push({ 0, 0 });
-  while (!q.empty()) {
-    ++steps;
-    queue<pair<int, int>> q1;
-    while (!q.empty()) {
-      auto c = q.front();
-      q.pop();
-      if (exchange(g[c.first][c.second], 1) == 1) continue;
-      if (c.first == g.size() - 1 && c.second == g.size() - 1) return steps;
-      for (auto i = c.first - 1; i <= c.first + 1; ++i)
-        for (auto j = c.second - 1; j <= c.second + 1; ++j)
-          if (i != c.first || j != c.second) {
-            if (i >= 0 && j >= 0 && i < g.size() && j < g.size() && !g[i][j]) {
-              q1.push({ i, j });
-            }
-          }
+    int drow[8] = {-1,-1,-1,0,1,1,1,0};
+    int dcol[8] = {-1,0,1,1,1,0,-1,-1};
+    int n;
+    bool valid(vector<vector<int>>& grid , int r , int c){
+        if(r<0 || c<0 || r==n || c==n) return 0;
+        if(grid[r][c]==1) return 0;
+        return 1;
     }
-    swap(q, q1);
-  }
-  return -1;
-}
-
+    
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        n = grid.size();
+        
+        if(grid[0][0]==1 || grid[n-1][n-1]==1) return -1;
+        if(n==1) return 1;
+        queue<pair<int,int>>q;
+        q.push({0,0});
+        int ans = 0;
+        bool flag = false;
+        while(!q.empty()){
+            int size = q.size();
+            ans++;
+            while(size-->0){
+                auto curr = q.front();
+                q.pop();
+                int row = curr.first;
+                int col = curr.second;
+                
+                for(int i = 0 ; i < 8 ; i++){
+                    for(int j = 0 ; j < 8 ; j++){
+                        int r = row + drow[i];
+                        int c = col + dcol[j];
+                        
+                        if(valid(grid,r,c)){
+                            if(r==n-1 && c == n-1){
+                                return ans+1;
+                            }
+                            grid[r][c] = 1;
+                            q.push({r,c});
+                        }
+                    }
+                }
+            }
+        }
+        
+        return -1;
+    }
 };
-
-
-
-
-
-
