ed8ba99910433d6476d5950e517ad9548268b04d
Kartik <67475518+BhinTatta@users.noreply.github.com>
2022-11-01 21:03:08 +0530

    Time: 49 ms (63.64%), Space: 17.2 MB (5.40%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -1,9 +1,15 @@
 class Solution {
+private:
+    vector<vector<int>> dp;
 public:
     int n , m;
+    
     vector<int> findBall(vector<vector<int>>& grid) {
         n = grid.size() , m = grid[0].size();
         vector<int> ans;
+        
+        dp.resize(101,vector<int>(101,-2));
+        
         for(int i = 0 ; i < m ; i++){
             int x = helper(grid , 0 , i );
             ans.push_back(x);
@@ -16,6 +22,8 @@ public:
         if(c<0 || c>=m) return -1;
         int ans = -1;
         
+        if(dp[r][c]!=-2) return dp[r][c]; 
+        
         if(grid[r][c]==1){
             if(c<m-1 && grid[r][c+1]==1){
                 ans = helper(grid , r+1 , c+1);
@@ -27,7 +35,7 @@ public:
             }
         }
         
-        return ans;
+        return dp[r][c] = ans;
     }
 };
 
