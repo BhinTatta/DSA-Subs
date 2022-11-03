56457ee98f51e4ae16ed547f6e161c099d9bea82
Kartik <67475518+BhinTatta@users.noreply.github.com>
2022-11-03 18:35:57 +0530

    Time: 300 ms (19.43%), Space: 56.5 MB (93.11%) - LeetHub

--- 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.cpp
+++ 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     int maximumGroups(vector<int>& nums) {
-        sort(nums.begin() , nums.end());
         int ans = 0;
-        
         int n = nums.size() , pre = 1;
         while(n>0 && n>=pre){
             ans++;
