4d43a3538d5caca4626e4815386e527be5a4758c
Kartik <67475518+BhinTatta@users.noreply.github.com>
2022-12-12 17:38:29 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (97.28%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int dp[46];
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=2;
-        for(int i = 3 ; i <= n ; i++){
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-        return dp[n];
+          if (n == 0 || n == 1) {
+            return 1;
+          }
+
+          int prev1 = 1;
+          int prev2 = 1;
+          int current = 0;
+
+          for (int i = 2; i <= n; i++) {
+            current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;
+          }
+
+          return current;
     }
 };
