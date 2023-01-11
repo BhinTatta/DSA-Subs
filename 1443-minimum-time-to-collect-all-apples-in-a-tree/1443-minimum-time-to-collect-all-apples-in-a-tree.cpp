c5adbfab2b616b1ab4a9b126e865e28bf88fe825
Kartik <67475518+BhinTatta@users.noreply.github.com>
2023-01-11 18:41:57 +0530

    Time: 187 ms (87.58%), Space: 65.5 MB (30.20%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
@@ -7,32 +7,26 @@ public:
             adj[a].push_back(b);
             adj[b].push_back(a);
         }
+        
         vector<int> vis(n,0);
+        
         int ans = helper(adj , has , vis , 0);
         
-        return ans==0 ? 0 : ans-2;
+        return ans == 0 ? 0 : ans-2;
     }
     
     int helper(vector<vector<int>>& adj , vector<bool>& has , vector<int> &vis , int node){
-        //cout<<node<<endl;
-        if(vis[node]) return 0;
-        vis[node] = 1;
-        
+        vis[node] = 1;      
         int ans = 0;
         
         for(auto it : adj[node]){
             if(!vis[it]) ans += helper(adj , has , vis , it);
         }
-        
+
         if(ans == 0 && has[node]){
-            //cout<<node<<" = "<<"2"<<endl;
             return 2;
         }
-        if(ans != 0 ){
-            //cout<<node<<" = "<<ans+2<<endl;
-            return ans+2;
-        }
-        //cout<<node<<" = "<<"0"<<endl;
-        return 0;
+
+        return ans == 0 ? 0 : ans+2;
     }
 };
