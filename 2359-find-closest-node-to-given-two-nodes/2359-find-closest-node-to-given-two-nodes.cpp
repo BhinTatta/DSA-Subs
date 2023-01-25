a586998adbbb989e42a3026facc1fb60628fa438
Kartik <67475518+BhinTatta@users.noreply.github.com>
2023-01-25 17:16:47 +0530

    Time: 185 ms (81.44%), Space: 97.5 MB (90.03%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
@@ -1,18 +1,14 @@
 class Solution {
 private:
     int n;
-    vector<int> vis1 , vis2;
+    
 public:
     int closestMeetingNode(vector<int>& edges, int node1, int node2) {
         int n = edges.size();
+        vector<int> vis1(n,-1) , vis2(n,-1);
         
-        vis1.resize(n,-1);
-        vis2.resize(n,-1);
-        // vis1[node1]=0;
-        // vis2[node2]=0;
-        
-        dfs(edges , node1, 0 , false);
-        dfs(edges , node2 , 0 , true);
+        dfs(edges , node1, 0 , vis1);
+        dfs(edges , node2 , 0 , vis2);
         
         int ans = INT_MAX , idx = -1;
         for(int i = 0 ; i < n ; i++){
@@ -26,15 +22,16 @@ public:
         return idx;
     }
     
-    void dfs(vector<int> &edges , int node , int d , bool state){
+    void dfs(vector<int> &edges , int node , int d , vector<int> &vis){
         
-        if(state && vis2[node]!=-1) return;
-        if(!state && vis1[node]!=-1) return;
-        if(state) vis2[node] = d;
-        if(!state) vis1[node] = d;
+        if(vis[node]!=-1) return;
+        
+        vis[node] = d;
+        
         if(edges[node]==-1){
             return;
         }
-        dfs(edges , edges[node] , d+1 , state);
+        
+        dfs(edges , edges[node] , d+1 , vis);
     }
 };
