1c96ac779fa260da464769e5fc5a3ce7814eef81
Kartik <67475518+BhinTatta@users.noreply.github.com>
2023-01-25 17:33:47 +0530

    Time: 9 ms (91.83%), Space: 10.9 MB (29.68%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,17 +4,12 @@ public:
         unordered_map<int,int> mp;
         vector<int> ans;
         for(int i = 0 ; i < nums.size() ; i++){
-            int curr = nums[i];
-            int req = target - curr;
+            int curr = nums[i] , req = target-curr;
             if(mp.find(req)!=mp.end()){
-                ans.push_back(i);
-                ans.push_back(mp[req]);
-                break;
+                return {i , mp[req]};
             }
             mp[curr] = i;
         }
-        
         return ans;
-        
     }
 };
