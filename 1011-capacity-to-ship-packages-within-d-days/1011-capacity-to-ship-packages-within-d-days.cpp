3445395adce05f093128f48f25550d8f20364ed8
Kartik <67475518+BhinTatta@users.noreply.github.com>
2023-02-22 17:35:59 +0530

    Time: 76 ms (37.03%), Space: 26.1 MB (37.41%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
-    int shipWithinDays(vector<int>& nums, int days) {
-        //sort(nums.begin() , nums.end());
-        int n = nums.size();
+    int shipWithinDays(vector<int>& nums, int days) {      
         int l = 1 , r = INT_MAX-2 , ans;
         
         while(l<r){
             int curr = (l+r)/2;
-            //cout<<l<<" "<<r<<" = "<<curr<<endl;
             bool done = helper(nums , days , curr);
             if(done) r = curr;
             else l = curr+1;
@@ -19,9 +16,11 @@ public:
     
     bool helper(vector<int> &nums , int d , int w){
         int day = 0 , n = nums.size() , i = 0;
+        
         while(i < n){
-            int curr = 0;     
             if(nums[i] > w) return false;
+            
+            int curr = 0;     
             while(i<n && curr+nums[i] <= w ){
                 curr += nums[i];
                 i++;
@@ -29,9 +28,8 @@ public:
             
             day++;
         }
-        // cout<<endl;
-        return day <= d;
         
+        return day <= d;        
     }
     
 };
