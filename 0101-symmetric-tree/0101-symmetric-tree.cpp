68476ac92bed9df89cacc8d11df27c0b7a9e9eb6
Kartik <67475518+BhinTatta@users.noreply.github.com>
2023-03-13 11:18:55 +0530

    Time: 0 ms (100.00%), Space: 16.5 MB (58.96%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -17,10 +17,7 @@ public:
     
     bool helper(TreeNode* a , TreeNode* b){
         if(!a && !b) return true;
-        if( (!a && b) || (a && !b)  ) return false;
-        
-        if(a->val != b->val) return false;
-        
+        if( (!a && b) || (a && !b) || (a->val != b->val)  ) return false;
         return helper(a->left , b->right) && helper(a->right , b->left);
     }
 };
