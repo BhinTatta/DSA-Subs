5c1a143da2f6a1721ca26ca48d1335a70a15fa6a
Kartik <67475518+BhinTatta@users.noreply.github.com>
2023-03-23 02:40:20 +0530

    Time: 486 ms (71.01%), Space: 138 MB (35.43%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-    int ans = INT_MAX;
     int minScore(int n, vector<vector<int>>& roads) {
         vector<vector<pair<int,int>>> adj(n+1);
         for(auto it : roads){
@@ -8,21 +7,50 @@ public:
             adj[a].push_back({b,c});
             adj[b].push_back({a,c});
         }
-        vector<int> vis(n+1 , 0);
-        helper(1 , adj , vis);
-        return ans;
         
+        vector<int> vis(n+1 , 0);
+        
+        int ans = INT_MAX;
+        
+        queue<int> q;
+        q.push(1);
+            
+        while(!q.empty()){
+            int curr = q.front();
+            q.pop();
+            
+           
+            
+            for(auto it : adj[curr]){
+                
+                int node = it.first , len = it.second;
+                ans = min(ans , len);
+                
+                if(!vis[node]){
+                    vis[node]=1;
+                    q.push(node);
+                }
+            }
+            
+        }
+        
+        return ans;
     }
     
-    void helper(int curr , vector<vector<pair<int,int>>> &adj , vector<int> &vis){
-        if(vis[curr]) return;
-        vis[curr] = 1;
-        
-        for(auto it : adj[curr]){
-            int node = it.first , dis = it.second;
-            ans = min(dis,ans);
-            if(!vis[node]) helper(node , adj , vis);
-        }
-        
-        }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 };
