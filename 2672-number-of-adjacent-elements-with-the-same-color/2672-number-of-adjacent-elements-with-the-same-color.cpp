7d7dd1de41d101d45696fa3ef3178f364bbee981
Kartik <67475518+BhinTatta@users.noreply.github.com>
2023-05-10 13:16:08 +0530

    Time: 588 ms (57.23%), Space: 175.6 MB (67.68%) - LeetHub

---
+++ 2672-number-of-adjacent-elements-with-the-same-color/2672-number-of-adjacent-elements-with-the-same-color.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> colorTheArray(int n, vector<vector<int>>& q) {
+        int count = 0 , m = q.size();
+        vector<int> dp(n+1,0);
+        vector<int> ans;
+        
+        for(int i = 0 ; i < m ; i++){
+            int ind = q[i][0] , c = q[i][1];
+            
+            int l = ind-1>=0 ? dp[ind-1] : 0;
+            int r = ind+1<n ? dp[ind+1] : 0;
+            
+            if(dp[ind] && l==dp[ind]) count--;
+            if(dp[ind] && r==dp[ind]) count--;
+            
+            dp[ind] = c;
+            
+            if(l==dp[ind]) count++;
+            if(r==dp[ind]) count++;
+
+            ans.push_back(count);
+        }
+        
+        return ans;
+    }
+};
