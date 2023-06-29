49ca4cf27b182b972b022a7d098814868a5900a4
Kartik <67475518+BhinTatta@users.noreply.github.com>
2023-06-30 02:15:17 +0530

    Time: 332 ms (85.77%), Space: 107.3 MB (89.21%) - LeetHub

---
+++ 2623-memoize/2623-memoize.ts
@@ -0,0 +1,27 @@
+type Fn = (...params: any) => any
+
+function memoize(fn: Fn): Fn {
+    const cache = new Map<string,any>();
+    return function(...args : any[]) : any {
+            const key = JSON.stringify(args);
+    if (cache.has(key)) {
+      return cache.get(key);
+    } else {
+      const result = fn(...args);
+      cache.set(key, result);
+      return result;
+    }
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
