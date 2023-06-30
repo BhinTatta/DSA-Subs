d428ec3f8b927537cc61df3067746e7925470dce
Kartik <67475518+BhinTatta@users.noreply.github.com>
2023-07-01 01:01:41 +0530

    Time: 57 ms (94.24%), Space: 41.5 MB (93.14%) - LeetHub

--- 2723-add-two-promises/2723-add-two-promises.js
+++ 2723-add-two-promises/2723-add-two-promises.js
@@ -4,8 +4,8 @@
  * @return {Promise}
  */
 var addTwoPromises = async function(promise1, promise2) {
-    const [a,b] = await Promise.all([promise1 , promise2]);
-    
+    let a = await promise1;
+    let b = await promise2;
     return a+b;
 };
 
