4a362806edd173700b2edb2a50418044b76e3e08
Kartik <67475518+BhinTatta@users.noreply.github.com>
2023-07-04 04:14:04 +0530

    Time: 2672 ms (10.89%), Space: 244 MB (13.89%) - LeetHub

---
+++ 2761-prime-pairs-with-target-sum/2761-prime-pairs-with-target-sum.cpp
@@ -0,0 +1,37 @@
+class Solution {
+private:
+    unordered_map<int,int> mp;
+public:
+    
+    void prime(int n){
+        int dp[n+1];
+        memset(dp,0,sizeof(dp));
+        for(int i = 2 ; i < n/2 ; i++){
+            int j = 2;
+            if(dp[i]) continue;
+            while(i*j < n){
+                dp[i*j]=1;
+                j++;
+            }
+        }
+        for(int i = 2 ; i <= n ; i++){
+            if(!dp[i]){
+                //cout<<i<<" "; 
+                mp[i]++;
+            }
+            
+        }
+    }
+    
+    vector<vector<int>> findPrimePairs(int n) {
+        prime(n);
+        vector<vector<int>> ans;
+        for(int i = 2 ; i <= n/2 ; i++){
+            int req = n-i;
+            if(mp.find(i)!=mp.end() && mp.find(req)!=mp.end()){
+                ans.push_back({i,req});
+            }
+        }
+        return ans;
+    }
+};
